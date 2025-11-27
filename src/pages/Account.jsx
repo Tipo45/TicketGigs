76de8fc80b856d6cd0eec28535a994067e500417
Azebi-Alex Steven .T. <tipo4542@gmail.com>
2025-11-27 22:28:13 +0100

    check

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -337,6 +337,7 @@ const Account = () => {
                                           className="cursor-pointer"
                                           onClick={(e) => {e.stopPropagation();}}
                                         >
+                                            
                                           <Trash2 className="h-4 w-4 mr-2" />
                                           Delete
                                         </Button>
